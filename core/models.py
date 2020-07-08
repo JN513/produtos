4c8eaaa554e3ac01cda89e4e183095f759e8ab02
JN513 <jn5.guape@gmail.com>
2020-07-08 20:05:06 -0300

    priemria versão das models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from datetime import datetime 
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(db_index=True, max_length=50,unique=True)
+    email = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=200)
+    username = models.CharField(max_length=25, unique=True)
+    datetime = models.DateTimeField(default=datetime.now,blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50, db_index=True)
+    descricao = models.TextField()
+    preco = models.IntegerField()
+    quantidade = models.IntegerField()
+    datadecriacao = models.DateField()
+    datademodificacao = models.DateField()
+    criador = models.ForeignKey(Usuario , on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria , on_delete=models.CASCADE)
+    tipo = models.ManyToManyField(Tipo)
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50, db_index=True)
+
+    def __str__(self):
+        return self.nome
+
+class Tipo(models.Model):
+    nome = models.CharField(max_length=50, db_index=True)
+    Categoria = models.ForeignKey(Categoria , on_delete=models.CASCADE)
